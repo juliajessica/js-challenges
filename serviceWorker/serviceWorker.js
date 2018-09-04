3e8d3c29a3d521cb3a0859414478c0b33640354b
julia sheremet <juliajessicasher@gmail.com>
2018-09-04 11:38:43 -0700

    more register to html file and add notes on needing an https setup

--- serviceWorker/serviceWorker.js
+++ serviceWorker/serviceWorker.js
@@ -1,13 +1,3 @@
 //https://medium.com/@addyosmani/progressive-web-apps-with-react-js-part-3-offline-support-and-network-resilience-c84db889162c
 
-// Check for browser support of service worker
-if ('serviceWorker' in navigator) {
- navigator.serviceWorker.register('service-worker.js')
- .then(function(registration) {
-   // Successful registration
-   console.log('Hooray. Registration successful, scope is:', registration.scope);
- }).catch(function(err) {
-   // Failed registration, service worker won’t be installed
-   console.log('Whoops. Service worker registration failed, error:', error);
- });
-}
+//https://developers.google.com/web/fundamentals/primers/service-workers/#install_a_service_worker
